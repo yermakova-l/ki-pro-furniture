f26478524aa4ada5fcf68812b568dbacee914a4b
yermakova-l <lyudmyla.yermakova@gmail.com>
2026-03-07 02:38:47 +0200

    Revert o version without Vite

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,15 @@
-// Імпорт головного файлу стилів (Vite сам зробить з них один файл)
-import '../styles/style.css';
-
 /* =========================================
    Mobile menu functionality
 ========================================= */
 import { initMobileMenu } from './modules/mobile-menu.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  // Ініціалізація мобільного меню
+  initMobileMenu();
+});
 /* =========================================
    Filtered Galleryfunctionality
 ========================================= */
 
 import { initGallery } from './modules/filter-gallery.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Ініціалізація мобільного меню
-  initMobileMenu();
-  initGallery();
-});
+initGallery();
