10e62f75d44bbed7904938139d7225b8adc9ad09
yermakova-l <lyudmyla.yermakova@gmail.com>
2026-03-11 17:00:29 +0200

    Mnemonials section added

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,12 @@
    Mobile menu functionality
 ========================================= */
 import { initMobileMenu } from './modules/mobile-menu.js';
+import { initGallery } from './modules/filter-gallery.js';
+import { initTestimonials } from './modules/testimonials.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Ініціалізація мобільного меню
-  initMobileMenu();
+  // Ініціалізація меню та галереї (твої модулі)
+  if (typeof initMobileMenu === 'function') initMobileMenu();
+  if (typeof initGallery === 'function') initGallery();
+  if (typeof initTestimonials === 'function') initTestimonials();
 });
-/* =========================================
-   Filtered Galleryfunctionality
-========================================= */
-
-import { initGallery } from './modules/filter-gallery.js';
-initGallery();
